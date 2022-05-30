c9d6779a6a8bf25dcbcb994dc96f224914eae927
sson22 <sophiesbson@gmail.com>
2022-05-30 19:26:47 +1000

    Extract Page info

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+var baseURL string = "https://au.indeed.com/jobs?q=software&limit=50"
 
 func main(){
-	fmt.Println("Hello")
+	pages := getPages()
+	fmt.Println(pages)
+
+}
+
+//Get pages using goquery
+func getPages() int {
+	res, err := http.Get(baseURL)
+	checkError(err)
+	checkStatusCode(res)
+
+	//Close function to prevent memory leak
+	defer res.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkError(err)
+	//Find div that has classname pagination from the URL
+	doc.Find(".pagination").Each()
+	return 0
+}
+
+//Check error and log the error
+func checkError(err error){
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+//Check status code and log the code if request was not fulfilled
+func checkStatusCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with Status: ", res.StatusCode)
+	}
 }

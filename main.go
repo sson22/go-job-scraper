c5cc9089158c2e43e71db59c906916e2e7d10208
sson22 <sophiesbson@gmail.com>
2022-05-30 21:21:25 +1000

    Extract job details

--- main.go
+++ main.go
@@ -5,24 +5,60 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
+type extractedJob struct {
+	id string
+	title string
+	location string
+	company string
+	salary string
+	description string
+}
 var baseURL string = "https://au.indeed.com/jobs?q=software&limit=50"
 
 func main(){
+	var totalJobs []extractedJob
 	totalPages := countPages()
-	fmt.Println(totalPages)
-
 	for i :=0; i < totalPages; i++ {
-		getPage(i)
+		extractedJobs :=getPage(i)
+		totalJobs = append(totalJobs, extractedJobs...)
 	}
+	fmt.Println(totalJobs)
 
 }
-func getPage(pageNumber int) {
+
+//Get each page and find each job-id
+func getPage(pageNumber int)[]extractedJob {
+	var jobs []extractedJob
 	pageURL := baseURL + "&start="+ strconv.Itoa(pageNumber*50)
-	fmt.Println(pageURL)
+	res, err := http.Get(pageURL)
+	checkError(err)
+	checkStatusCode(res)
+	//Close function to prevent memory leak
+	defer res.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkError(err)
+	//Find id of each job and extract job information
+	searchCards := doc.Find(".cardOutline")
+	searchCards.Each(func(i int, card *goquery.Selection){
+		job := extractJob(card)
+		jobs = append(jobs, job)
+	})
+	return jobs
+}
+//Extract job information
+func extractJob(card *goquery.Selection) extractedJob{
+	id, _ := card.Find(".jcs-JobTitle").Attr("data-jk")
+	title := cleanString(card.Find(".jcs-JobTitle>span").Text())
+	location := cleanString(card.Find(".companyLocation").Text())
+	company := cleanString(card.Find(".companyName").Text())
+	salary := cleanString(card.Find(".salary-snippet-container>.attribute_snippet").Text())
+	description := cleanString(card.Find(".job-snippet>ul>li").Text())
+	return extractedJob{id:id, title:title, location:location, company:company, salary: salary, description:description}
 }
 //Get number of pages to inspect on URL using goquery
 func countPages() int {
@@ -42,6 +78,10 @@ func countPages() int {
 	return pages
 }
 
+//Trim empty string
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
+}
 //Check error and log the error
 func checkError(err error){
 	if err != nil {
@@ -54,3 +94,4 @@ func checkStatusCode(res *http.Response) {
 		log.Fatalln("Request failed with Status: ", res.StatusCode)
 	}
 }
+
